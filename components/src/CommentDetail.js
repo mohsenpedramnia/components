0fab0ba2daa4073afe6713c164c40247f6fb2b18
mohsenpedramnia <mohsen.pedramnia@gmail.com>
2019-09-01 17:23:50 +0430

    nest first component

---
+++ components/src/CommentDetail.js
@@ -0,0 +1,21 @@
+import React from "react";
+import faker from "faker";
+
+const CommentDetail = () => {
+  return (
+    <div className="comment">
+      <a href="/" className="avatar">
+        <img alt="avatar" src={faker.image.avatar()} />
+      </a>
+      <div className="content">
+        <a className="author">Sam</a>
+        <div className="metadata">
+          <span className="data">Today at 6:00PM</span>
+        </div>
+        <div className="text">Nice blog post!</div>
+      </div>
+    </div>
+  );
+};
+
+export default CommentDetail;
--- components/src/index.js
+++ components/src/index.js
@@ -1,22 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
+import CommentDetail from "./CommentDetail";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a className="author">Sam</a>
-          <div className="metadata">
-            <span className="data">Today at 6:00PM</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
     </div>
   );
 };
