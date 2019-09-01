dd9327d54c01539d2f18805ab96b11c2559622a3
mohsenpedramnia <mohsen.pedramnia@gmail.com>
2019-09-01 18:19:19 +0430

    Using props and remove hard code

--- components/src/CommentDetail.js
+++ components/src/CommentDetail.js
@@ -1,18 +1,18 @@
 import React from "react";
 import faker from "faker";
 
-const CommentDetail = () => {
+const CommentDetail = props => {
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src={faker.image.avatar()} />
+        <img alt="avatar" src={props.avatar} />
       </a>
       <div className="content">
-        <a className="author">Sam</a>
+        <a className="author">{props.author}</a>
         <div className="metadata">
-          <span className="data">Today at 6:00PM</span>
+          <span className="data">{props.timeAgo}</span>
         </div>
-        <div className="text">Nice blog post!</div>
+        <div className="text">{props.content}</div>
       </div>
     </div>
   );
--- components/src/index.js
+++ components/src/index.js
@@ -6,9 +6,24 @@ import CommentDetail from "./CommentDetail";
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:40PM"
+        content="Nice blog post!"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 2:40PM"
+        content="Good post!"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Yesterday at 4:40PM"
+        content="I like subject"
+        avatar={faker.image.avatar()}
+      />
     </div>
   );
 };
