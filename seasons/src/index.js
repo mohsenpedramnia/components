e14131b18bd0fb3741551e0628684d9d11e5117b
mohsenpedramnia <mohsen.pedramnia@gmail.com>
2019-09-03 20:07:06 +0430

    Extracting option to config objects

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -6,7 +6,7 @@ class App extends React.Component {
   state = { lat: null, errorMessage: "" };
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      position => this.setState({ lat: position.coords.longitude }),
+      position => this.setState({ lat: position.coords.latitude }),
 
       err => this.setState({ errorMessage: err.message })
     );
