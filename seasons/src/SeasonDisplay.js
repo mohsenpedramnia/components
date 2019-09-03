6fe700972650a8aa365d7fdf17b25d35a5d508b8
mohsenpedramnia <mohsen.pedramnia@gmail.com>
2019-09-03 18:34:45 +0430

    Using ternary expression in SeasonDisplay jsx

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,7 +1,23 @@
 import React from "react";
 
-const SeasonDisplay = () => {
-  return <div>Display season</div>;
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+
+const SeasonDisplay = props => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const text =
+    season === "winter" ? "Burr, it is chilly" : "lets hit the beach";
+  console.log(season);
+  return (
+    <div>
+      <h1>{text}</h1>
+    </div>
+  );
 };
 
 export default SeasonDisplay;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
   state = { lat: null, errorMessage: "" };
@@ -17,7 +18,7 @@ class App extends React.Component {
       return <div>Error:{this.state.errorMessage}</div>;
     }
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
     return <div>Loading</div>;
   }
