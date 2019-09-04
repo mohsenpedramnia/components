2d6fb5ee78d07ce694f038ca008127eacacba1bd
mohsenpedramnia <mohsen.pedramnia@gmail.com>
2019-09-04 18:45:54 +0430

    Handeling form submittal and preventDefault

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -2,19 +2,19 @@ import React from "react";
 
 class SearchBar extends React.Component {
   state = { term: "" };
-
+  onFormSubmit(event) {
+    event.preventDefault();
+  }
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
             <input
               type="text"
               value={this.state.term}
-              onChange={e =>
-                this.setState({ term: e.target.value.toUpperCase() })
-              }
+              onChange={e => this.setState({ term: e.target.value })}
             />
           </div>
         </form>
