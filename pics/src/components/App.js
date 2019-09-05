a24e3e9984f42a2f200f0420577d9849676bbf00
mohsenpedramnia <mohsen.pedramnia@gmail.com>
2019-09-05 16:59:43 +0430

    Using axios for image request

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID 11ad1a90fb03fccddcd2d560f903445e1b2577177c44be9d3fdf769c50e0a27c"
+      }
+    });
   }
 
   render() {
