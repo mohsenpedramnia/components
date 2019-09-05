82d82faa285b347cbe2174b5db917d25a697e625
mohsenpedramnia <mohsen.pedramnia@gmail.com>
2019-09-05 17:17:32 +0430

    First method to get respond from Async request

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -4,13 +4,17 @@ import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    axios.get("https://api.unsplash.com/search/photos", {
-      params: { query: term },
-      headers: {
-        Authorization:
-          "Client-ID 11ad1a90fb03fccddcd2d560f903445e1b2577177c44be9d3fdf769c50e0a27c"
-      }
-    });
+    axios
+      .get("https://api.unsplash.com/search/photos", {
+        params: { query: term },
+        headers: {
+          Authorization:
+            "Client-ID 11ad1a90fb03fccddcd2d560f903445e1b2577177c44be9d3fdf769c50e0a27c"
+        }
+      })
+      .then(response => {
+        console.log(response.data.results);
+      });
   }
 
   render() {
