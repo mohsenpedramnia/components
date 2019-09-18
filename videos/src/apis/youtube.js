995103225f8fafbfd9b8ac7077e9b1f4d69c88d7
mohsenpedramnia <mohsen.pedramnia@gmail.com>
2019-09-18 19:15:31 +0430

    Successfully  made a request to youtube API

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -2,11 +2,12 @@ import axios from "axios";
 
 const KEY = "AIzaSyBm8ZIJC1CDM8d3Z8xuU7RH7qIqE7xS0Cg";
 
+export const baseParams = {
+  part: "snippet",
+  maxResults: 5,
+  key: KEY
+};
+
 export default axios.create({
-  baseURL: "https://www.googleapis.com/youtube/v3",
-  params: {
-    part: "snippet",
-    maxResults: 5,
-    key: KEY
-  }
+  baseURL: "https://www.googleapis.com/youtube/v3"
 });
--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import youtube, { baseParams } from "../apis/youtube";
 
 class App extends React.Component {
+  onTermSubmit = term => {
+    youtube.get("/search", {
+      params: {
+        ...baseParams,
+        q: term
+      }
+    });
+  };
   render() {
     return (
       <div className="ui container">
-        <SearchBar />
+        <SearchBar onFormTermSubmit={this.onTermSubmit} />
       </div>
     );
   }
