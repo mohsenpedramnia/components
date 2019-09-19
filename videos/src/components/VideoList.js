e0c6c4f3c4db61ecd0adf9d7dfd1811398b27cfd
mohsenpedramnia <mohsen.pedramnia@gmail.com>
2019-09-19 12:52:12 +0430

    Styling  list of video items

--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -5,7 +5,7 @@ const VideoList = ({ videos }) => {
   const renderdList = videos.map(video => {
     return <VideoItem video={video} />;
   });
-  return <div>{renderdList}</div>;
+  return <div className="ui relaxed divided list">{renderdList}</div>;
 };
 
 export default VideoList;
