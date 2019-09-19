be0a606b155a8df4363271e2283dbe1a8f33b858
mohsenpedramnia <mohsen.pedramnia@gmail.com>
2019-09-19 19:29:30 +0430

    Fixing a few warning and using semantic grid ui

--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -3,7 +3,13 @@ import VideoItem from "./VideoItem";
 
 const VideoList = ({ videos, onVideoSelect }) => {
   const renderdList = videos.map(video => {
-    return <VideoItem onVideoSelect={onVideoSelect} video={video} />;
+    return (
+      <VideoItem
+        key={video.id.videoId}
+        onVideoSelect={onVideoSelect}
+        video={video}
+      />
+    );
   });
   return <div className="ui relaxed divided list">{renderdList}</div>;
 };
