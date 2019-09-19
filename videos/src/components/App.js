95d4e2e7a7e34f5f9e8278aa0081f1d186d285fc
mohsenpedramnia <mohsen.pedramnia@gmail.com>
2019-09-19 17:50:40 +0430

    Conditional Rendering

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import youtube, { baseParams } from "../apis/youtube";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
@@ -16,12 +17,13 @@ class App extends React.Component {
   };
 
   onVideoSelect = video => {
-    console.log("from the App!", video);
+    this.setState({ selectedVideo: video });
   };
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormTermSubmit={this.onTermSubmit} />
+        <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={this.onVideoSelect}
           videos={this.state.videos}
---
+++ videos/src/components/VideoDetail.js
@@ -0,0 +1,8 @@
+import React from "react";
+const VideoDetail = ({ video }) => {
+  if (!video) {
+    return <div>Loading...</div>;
+  }
+  return <div>{video.snippet.title}</div>;
+};
+export default VideoDetail;
