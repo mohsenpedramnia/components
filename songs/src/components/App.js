906dca323006dad84b61f1bc48748f623e2bf182
mohsenpedramnia <mohsen.pedramnia@gmail.com>
2019-09-21 18:41:45 +0430

    Add connect function

--- songs/src/components/App.js
+++ songs/src/components/App.js
@@ -1,7 +1,11 @@
 import React from "react";
-
+import SongList from "./Songlist";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <SongList />
+    </div>
+  );
 };
 
 export default App;
