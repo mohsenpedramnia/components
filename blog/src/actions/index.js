3f15ce5beef33992763604639197838c272976a3
mohsenpedramnia <mohsen.pedramnia@gmail.com>
2019-09-23 11:53:41 +0330

    Shortened syntax with redux thunk for async await

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,5 +1,5 @@
-export const fetchPosts = () => {
-  return {
-    type: "FETCH_POSTS"
-  };
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonPlaceholder.get("/posts");
+  dispatch({ type: "FETCH_POST", payload: response });
 };
