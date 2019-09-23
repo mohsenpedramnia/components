b41be894225a53a3bc08f153937203b1930787ad
mohsenpedramnia <mohsen.pedramnia@gmail.com>
2019-09-23 17:30:55 +0330

    Switch statement in redux

--- blog/src/reducers/index.js
+++ blog/src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from "redux";
-
+import postsReducer from "../reducers/postsReducer";
 export default combineReducers({
-  replaceMe: () => "hi there"
+  posts: postsReducer
 });
