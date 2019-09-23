fcb19e4e1b039c3f81e4c16ea0bf05fc5d814e0e
mohsenpedramnia <mohsen.pedramnia@gmail.com>
2019-09-24 00:34:21 +0330

    Find relevent user

--- blog/src/reducers/index.js
+++ blog/src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from "redux";
 import postsReducer from "./postsReducer";
+import usersReducer from "./usersReducer";
 export default combineReducers({
-  posts: postsReducer
+  posts: postsReducer,
+  users: usersReducer
 });
