f657399f0fe6c57d520b6a54287c1842e7c000fd
mohsenpedramnia <mohsen.pedramnia@gmail.com>
2019-09-24 18:55:40 +0330

    Finding unique user ids

--- blog/src/components/UserHeader.js
+++ blog/src/components/UserHeader.js
@@ -1,12 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
-import { fetchUser } from "../actions";
 
 class UserHeader extends React.Component {
-  componentDidMount() {
-    this.props.fetchUser(this.props.userId);
-  }
-
   render() {
     const { user } = this.props;
     if (!user) {
@@ -19,7 +14,4 @@ class UserHeader extends React.Component {
 const mapStateToProps = (state, ownProps) => {
   return { user: state.users.find(user => user.id === ownProps.userId) };
 };
-export default connect(
-  mapStateToProps,
-  { fetchUser }
-)(UserHeader);
+export default connect(mapStateToProps)(UserHeader);
