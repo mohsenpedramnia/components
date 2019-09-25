d273e221ef3491c6faa162a8e77612a42a563315
mohsenpedramnia <mohsen.pedramnia@gmail.com>
2019-09-25 18:21:40 +0330

    Componet scaffolding and install react-router-dom

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
+const PageOne = () => {
+  return (
+    <div>
+      PageOne
+      <Link to="/pagetwo">navigate to page to</Link>
+    </div>
+  );
+};
+const PageTwo = () => {
+  return (
+    <div>
+      PageTwo
+      <button>click me!</button>
+      <Link to="/">navigate to page one</Link>
+    </div>
+  );
+};
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={PageOne} />
+          <Route path="/pagetwo" component={PageTwo} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
