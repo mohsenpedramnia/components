dbb86d429c295692289ca8ba98af3e7886765ea5
mohsenpedramnia <mohsen.pedramnia@gmail.com>
2019-09-25 18:55:25 +0330

    Wiring up routes

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,30 +1,21 @@
 import React from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
+import StreamCreate from "./screams/StreamCreate";
+import StreamDelete from "./screams/StreamDelete";
+import StreamEdit from "./screams/StreamEdit";
+import StreamList from "./screams/StreamList";
+import StreamShow from "./screams/StreamShow";
 
-const PageOne = () => {
-  return (
-    <div>
-      PageOne
-      <Link to="/pagetwo">navigate to page to</Link>
-    </div>
-  );
-};
-const PageTwo = () => {
-  return (
-    <div>
-      PageTwo
-      <button>click me!</button>
-      <Link to="/">navigate to page one</Link>
-    </div>
-  );
-};
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={PageOne} />
-          <Route path="/pagetwo" component={PageTwo} />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
