66818e81e92417e9167622d81084e2b613eed73b
mohsenpedramnia <mohsen.pedramnia@gmail.com>
2019-09-28 00:54:08 +0330

    Recording the userId

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -1,7 +1,8 @@
 import { SIGN_IN, SIGN_OUT } from "./types";
-export const signIn = () => {
+export const signIn = userId => {
   return {
-    type: SIGN_IN
+    type: SIGN_IN,
+    payload: userId
   };
 };
 
