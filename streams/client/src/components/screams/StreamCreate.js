b3680ab524e22254a5e1575e779e3e44f3c4429c
mohsenpedramnia <mohsen.pedramnia@gmail.com>
2019-09-30 19:14:52 +0330

    Creating streams action creator with REST conv

--- streams/client/src/components/screams/StreamCreate.js
+++ streams/client/src/components/screams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
+import { connect } from "react-redux";
+import { createStream } from "../../actions";
 
 class StreamCreate extends React.Component {
   renderError({ error, touched }) {
@@ -22,9 +24,9 @@ class StreamCreate extends React.Component {
       </div>
     );
   };
-  onSubmit(formValues) {
-    console.log(formValues);
-  }
+  onSubmit = formValues => {
+    this.props.createStream(formValues);
+  };
   render() {
     return (
       <form
@@ -53,7 +55,13 @@ const validate = formValues => {
   }
   return errors;
 };
-export default reduxForm({
+
+const formWrapped = reduxForm({
   form: "streamCreate",
   validate
 })(StreamCreate);
+
+export default connect(
+  null,
+  { createStream }
+)(formWrapped);
