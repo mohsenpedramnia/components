7ae8c61fd2c3698d9138bb2483f8f9876b31454a
mohsenpedramnia <mohsen.pedramnia@gmail.com>
2019-10-02 19:41:10 +0330

    Refactoring StreamEdit with initialValues

--- streams/client/src/components/screams/StreamEdit.js
+++ streams/client/src/components/screams/StreamEdit.js
@@ -1,15 +1,31 @@
+import _ from "lodash";
 import React from "react";
 import { connect } from "react-redux";
-import { fetchStream } from "../../actions";
+import { fetchStream, editStream } from "../../actions";
+import StreamForm from "./StreamForm";
+
 class StreamEdit extends React.Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
+
+  onSubmit = formValues => {
+    this.props.editStream(this.props.match.params.id, formValues);
+  };
+
   render() {
     if (!this.props.stream) {
       return <div>Loading...</div>;
     }
-    return <div>{this.props.stream.title}</div>;
+    return (
+      <div>
+        <h3>Edit a Stream</h3>
+        <StreamForm
+          initialValues={_.pick(this.props.stream, "title", "description")}
+          onSubmit={this.onSubmit}
+        />
+      </div>
+    );
   }
 }
 const mapStateToProps = (state, ownProps) => {
@@ -17,5 +33,5 @@ const mapStateToProps = (state, ownProps) => {
 };
 export default connect(
   mapStateToProps,
-  { fetchStream }
+  { fetchStream, editStream }
 )(StreamEdit);
