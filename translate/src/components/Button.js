6858b4ea418f7bbfc71d99d0b2d7be3cb2ecbe40
mohsenpedramnia <mohsen.pedramnia@gmail.com>
2019-10-05 23:51:37 +0330

    A touch more  setup

---
+++ translate/src/components/Button.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Button extends React.Component {
+  render() {
+    return <button className="ui button primary">submit</button>;
+  }
+}
+
+export default Button;
---
+++ translate/src/components/Field.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Field extends React.Component {
+  render() {
+    return (
+      <div className="ui field">
+        <label>name</label>
+        <input />
+      </div>
+    );
+  }
+}
+
+export default Field;
