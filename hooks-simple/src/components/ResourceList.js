a2bd54bc1eaa2b6bb29a2821c2b24f9b56bec9cf
mohsenpedramnia <mohsen.pedramnia@gmail.com>
2019-10-09 23:17:45 +0330

    Extracted Hook related logic into a function

--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -1,17 +1,20 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const ResourceList = ({ resource }) => {
+const useResources = resource => {
   const [resources, setResources] = useState([]);
   useEffect(() => {
     (async resource => {
       const response = await axios.get(
         `https://jsonplaceholder.typicode.com/${resource}`
       );
-
       setResources(response.data);
     })(resource);
   }, [resource]);
+  return resources;
+};
+const ResourceList = ({ resource }) => {
+  const resources = useResources(resource);
 
   return (
     <ul>
