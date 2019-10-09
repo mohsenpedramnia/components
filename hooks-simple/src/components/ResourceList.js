1945126a19552844e652beaf97a20e93edd4fc3f
mohsenpedramnia <mohsen.pedramnia@gmail.com>
2019-10-09 22:42:02 +0330

    Quick Gotcha with useEffect

--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -3,15 +3,14 @@ import axios from "axios";
 
 const ResourceList = ({ resource }) => {
   const [resources, setResources] = useState([]);
-  const fetchResource = async resource => {
-    const response = await axios.get(
-      `https://jsonplaceholder.typicode.com/${resource}`
-    );
-
-    setResources(response.data);
-  };
   useEffect(() => {
-    fetchResource(resource);
+    (async resource => {
+      const response = await axios.get(
+        `https://jsonplaceholder.typicode.com/${resource}`
+      );
+
+      setResources(response.data);
+    })(resource);
   }, [resource]);
 
   return <div>{resources.length}</div>;
