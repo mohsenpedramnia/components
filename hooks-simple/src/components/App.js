c180d9e0b9640280b14159c0e56af948842cbdce
mohsenpedramnia <mohsen.pedramnia@gmail.com>
2019-10-09 18:33:22 +0330

    Building statefull class components

--- hooks-simple/src/components/App.js
+++ hooks-simple/src/components/App.js
@@ -1,8 +1,27 @@
 import React from "react";
 
 class App extends React.Component {
+  state = { resource: "posts" };
   render() {
-    return <div>App</div>;
+    return (
+      <div className="ui container">
+        <div>
+          <button
+            onClick={() => this.setState({ resource: "posts" })}
+            className="ui green button"
+          >
+            Post
+          </button>
+          <button
+            onClick={() => this.setState({ resource: "todos" })}
+            className="ui pink button"
+          >
+            Todos
+          </button>
+        </div>
+        {this.state.resource}
+      </div>
+    );
   }
 }
 
